13bcf5d603a9b0f9dfcc01138109aac6fe2227c1
Pietra Ferreira <pietra.ferreira@embecosm.com>
2022-06-29 12:25:01 +0100

    Got logging to work properly

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 extern crate chrono;
 use chrono::*;
-use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::Write;
 use std::io;
 
-fn log_time(filename: &'static str) -> io::Result<()>{
+fn formatted_time_entry() -> String {
     let local: DateTime<Local> = Local::now();
     let formatted = local.format("%a, %d %b %Y %I:%M:%S %p\n").to_string();
-    let bytes = formatted.as_bytes();
-    let mut file = File::create(filename).unwrap();
+    formatted
+}
+
+fn record_entry_in_log(filename: &str, bytes: &[u8]) -> io::Result<()> {
+    let mut file = (OpenOptions::new().
+                   append(true). // append if file exists, create otherwise
+                   write(true).
+                   create(true).
+                   open(filename))?;
     file.write_all(bytes).unwrap();
+    Ok(())
+}
+
+fn log_time(filename: &'static str) -> io::Result<()>{
+    let entry = formatted_time_entry();
+    let bytes = entry.as_bytes();
+
+    record_entry_in_log(filename, &bytes)?;
     Ok(())
 }
 
