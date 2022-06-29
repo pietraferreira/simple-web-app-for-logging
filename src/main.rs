04956d394a94d90dfa78554e1d40b9bf8dc6330b
Pietra Ferreira <pietra.ferreira@embecosm.com>
2022-06-29 12:05:15 +0100

    How to write to a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::fs::File;
+use std::io::Write;
+use std::io;
+
+fn log_something(filename: &'static str, string: &'static [u8; 10]) -> io::Result<()>{
+    let mut file = File::create(filename).unwrap();
+    file.write_all(string).unwrap();
+    Ok(())
+}
 
 fn main() {
-    match File::create("foo.txt") {
+    match log_something("log.txt", b"I'm ALIVE!") {
         Ok(..) => println!("Created file"),
         Err(..) => println!("Failed to create file")
     }
